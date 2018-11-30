fa9c9eed7649cd828ea33fd4bbb127c6c2647e06
G.J.G. Hendriks <s2410540@fwn-bborg-edu-74-49.fmnsedu.rug.nl>
2018-11-30 16:36:55 +0100

    Removed the .class file of hello world. And started a tiny GUI

--- flora Scripts/helloWorld.java
+++ flora Scripts/helloWorld.java
@@ -1,24 +1,59 @@
-/** helloWorld.java, author: Marnix Jansma, 23-11-18 */
+/**
+ * 	helloWorld.java
+ * 	authors:
+ *  Marnix Jansma, 23-11-18
+ *  Gijs Hendriks 
+ *  
+ *  To compile:
+ *  javac -classpath "$FLORADIR/java/flora2java.jar":"$FLORADIR/java/interprolog.jar" helloWorld.java
+ *  
+ *  To run:
+ *  java -DPROLOGDIR=$PROLOGDIR -DFLORADIR=$FLORADIR -classpath $FLORADIR/java/flora2java.jar:$FLORADIR/java/interprolog.jar:./ helloWorld
+ */
 
-// To compile: javac -classpath "$FLORADIR/java/flora2java.jar":"$FLORADIR/java/interprolog.jar" helloWorld.java
 
-/* java 
-  -DPROLOGDIR=$PROLOGDIR 
-  -DFLORADIR=$FLORADIR 
-  -classpath $FLORADIR/java/flora2java.jar:$FLORADIR/java/interprolog.jar./flora\ Scripts/ 
-  helloWorld
-*/
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 import net.sf.flora2.API.*;
 import net.sf.flora2.API.util.*;
+import javax.swing.*;	//swing
 
 public class helloWorld{
 	
-  public static void main(String[] args) {
-		
-  FloraSession session = new FloraSession();
-	System.out.println("Flora-2 session started");
-	session.close();
-	System.exit(0);
-    }
+	public static void main(String[] args) {
+				
+			FloraSession session = new FloraSession();
+			System.out.println("Flora-2 session started");
+			System.out.println("Starting Gui");
+			initGUI();
+		}
+	
+	
+	
+	public static void initGUI(){
+		JFrame frame = new JFrame("Breast cancer risk assesment");
+	    GridLayout layout = new GridLayout(5,0);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    
+	    
+	    
+	    JButton nextButton = new JButton("Click me");
+	    nextButton.addActionListener(new ActionListener()
+	    {
+	    	  public void actionPerformed(ActionEvent e)
+	    	  {
+	    	    System.out.println("clicked a button");
+	    	  }
+	    	});
+	    
+	    frame.add(nextButton);
+	    frame.setLayout(layout);
+	    frame.setSize(1000, 500);
+	    frame.setLocation(300, 300);
+	    frame.setVisible(true);
+	    
+	}
+
 }
