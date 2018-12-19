351771f60b6f55a9c99e76059316e80502544106
G.J.G. Hendriks <s2410540@fwn-bborg-edu-74-97.fmnsedu.rug.nl>
2018-12-19 11:50:16 +0100

    Finished the method to read the questions from xml file.

--- src/java/KTPFrame.java
+++ src/java/KTPFrame.java
@@ -53,9 +53,9 @@ public class KTPFrame extends JFrame {
 		
 	    
 	    
-		readQuestions();
+		//readQuestions();
 	    
-	    
+		this.readQuestionsXML();
 
 
 		
@@ -84,7 +84,7 @@ public class KTPFrame extends JFrame {
 	    this.setVisible(true);
 	    
 	    
-	    this.readQuestionsXML();
+	    
 	    
 	}
 	
@@ -191,7 +191,7 @@ public class KTPFrame extends JFrame {
 	      }*/
 		
 		try{
-			File inputFile = new File("src/resource/questions.xml");
+			File inputFile = new File("src/resource/questions.xml");			// new file
 	        SAXBuilder saxBuilder = new SAXBuilder();
 	        Document document = saxBuilder.build(inputFile);
 	        Element classElement = document.getRootElement();					// get root
@@ -199,9 +199,9 @@ public class KTPFrame extends JFrame {
 	        List<Element> qList = classElement.getChildren();					// get list of questions
 	        System.out.println("Found " + qList.size() + " questions in " + inputFile.getAbsolutePath());
 	        
-	        for(int temp = 0; temp< qList.size(); temp++){
+	        for(int temp = 0; temp< qList.size(); temp++){						// for every question
 	        	Element q = qList.get(temp);
-	        	// make a new question
+	        	// make a new question object
 	        	String keyword = q.getChild("keyword").getText();
 	        	String question = q.getChild("question").getText();
 	        	String extra = null;
@@ -214,36 +214,64 @@ public class KTPFrame extends JFrame {
 	        	
 
 	        	
-	        	
+	        	// if attribute inputtype == spinner
 	        	if(q.getAttribute("inputtype").toString().equals(new Attribute("inputtype", "spinner").toString())){
 	        		//System.out.println("spinner");
-	        		String[] strings = q.getChild("spinner").getText().split(",");
-	        		int[] ints = new int[4];
+	        		String[] strings = q.getChild("spinner").getText().split(",");		// split into strings
+	        		int[] ints = new int[4];											// make new int array
 	        		int i = 0;
 	        		for(String str: strings){
-	        			ints[i] = Integer.parseInt(str); 
+	        			ints[i] = Integer.parseInt(str); 				// parse strings to ints
 	        			i++;
 	        		}
 	        		
+	        		//make new spinner
 	        		SpinnerModel model = new SpinnerNumberModel(ints[0], ints[1], ints[2], ints[3]);     
 	        		KTPJSpinner spinner = new KTPJSpinner(model);
 	        		componentList.add(spinner);
 	        		
+	        		
+	        	// if attribute inputtype == checkbox
 	        	}else if(q.getAttribute("inputtype").toString().equals(new Attribute("inputtype", "checkbox").toString())){
 	        		//System.out.println("checkbox");
 	        		
+	        		List<Element> list = q.getChildren("checkbox"); 				// get all checkboxes
+	        		for (Element e : list){					// for each checkbox
+	        			//System.out.println(e.toString());
+	        			KTPJCheckBox but = new KTPJCheckBox(e.getText());			// make new checkbox
+	        			componentList.add(but);										// add checkbox to the component list
+	        		}
+	        		
+	        		
+	        		
+	        		
+	        	
+	        	// if attribute inputtype == radiobutton
 	        	}else if(q.getAttribute("inputtype").toString().equals(new Attribute("inputtype", "radiobutton").toString())){
 	        		//System.out.println("radiobutton");
 	        		
+	        		List<Element> list = q.getChildren("radiobutton"); 				// get all radiobuttons
+	        		ButtonGroup group = new ButtonGroup();							// make new button group
+	        		for (Element e : list){					// for each checkbox
+	        			//System.out.println(e.toString());
+	        			KTPJRadioButton but = new KTPJRadioButton(e.getText());		// make new button
+	        			group.add(but);												// add button to the button group
+	        			componentList.add(but);										// add button to the component list
+	        		}
+	        		
+	        		
+	        		
 	        	}else{
-	        		System.out.println("Something went wrong");
+	        		System.err.println("Something went wrong in parsing the input types in the xml file.");
 	        	}
 	        	
 	        	
 	        	
 	        	
 	        	
-	        	Question newq = new Question(keyword, question, extra, componentList);
+	        	Question newq = new Question(keyword, question, extra, componentList);			// construct the new question
+	        	this.questionsList.add(newq);
+	        	
 	        	//String keyword, String question, String extra, List<KTPJComponent> componentList
 	        }
 	        
